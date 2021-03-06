23e36949762b033b02206978640ec1a9463415a1
VayerMaking <martin.vayer@gmail.com>
2021-03-06 22:53:21 +0200

    heroku idk

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask
 from flask import render_template, request, flash, redirect, url_for, session
 from flask_sqlalchemy import SQLAlchemy
 import time
+import os
 from datetime import datetime
 
 
@@ -17,9 +18,6 @@ def index():
     return render_template('index.html')
 
 
-
-if __name__ == '__main__':
-    db.create_all()
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host='0.0.0.0', port=port)
+port = int(os.environ.get("PORT", 5000))
+app.run(host='0.0.0.0', port=port, debug=True)
 	#app.run(debug=True)
