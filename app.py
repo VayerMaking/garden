0c968204c6ecb17e24943cf35b2cc1a51af19003
VayerMaking <martin.vayer@gmail.com>
2021-03-06 22:28:28 +0200

    heroku idk

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ import time
 from datetime import datetime
 import config
 
+
 app = Flask(__name__)
 app.config['SECRET_KEY'] = config.secret_key
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///todo.db'
@@ -14,6 +15,10 @@ db = SQLAlchemy(app)
 def index():
     return render_template('index.html')
 
+
+
 if __name__ == '__main__':
 	db.create_all()
-	app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
+	#app.run(debug=True)
