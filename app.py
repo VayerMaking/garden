ac4d445882329ad6babf493a4ae83c7190afb265
VayerMaking <martin.vayer@gmail.com>
2021-03-07 11:51:32 +0200

    working adding data to db and redirecting to a succesfull preorder page

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ from flask_sqlalchemy import SQLAlchemy
 import time
 import os
 from datetime import datetime
+#import hashlib
 
 
 app = Flask(__name__)
@@ -13,6 +14,18 @@ app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', None)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///todo.db'
 db = SQLAlchemy(app)
 
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    name = db.Column(db.String(100))
+    email = db.Column(db.String(100))
+    address = db.Column(db.String(150))
+
+    #def verify(self, thing_to_verify):
+    #    return self.thing_to_verify == hash_password(thing_to_verify)
+
+#def hash(thing_to_hash):
+#    return hashlib.sha256(thing_to_hash.encode('utf-8')).hexdigest()
+
 @app.route('/', methods=['GET'])
 def index():
     return render_template('index.html')
@@ -21,10 +34,21 @@ def index():
 def paypal():
     return render_template('paypal.html')
 
-@app.route('/preorder', methods=['GET'])
+@app.route('/preorder', methods=['GET', 'POST'])
 def preorder():
-    return render_template('preorder.html')
+    if request.method == "POST":
+        data = request.form
+        print(data['name'], data['email'], data['address'])
+        user = User(name=data['name'], email=data['email'], address=data['address'])
+        db.session.add(user)
+        db.session.commit()
 
+        return render_template('succesfull_preorder.html', name=user.name)
+    elif request.method == "GET":
+
+        return render_template("preorder.html")
+    #return render_template('preorder.html')
+db.create_all()
 port = int(os.environ.get("PORT", 5000))
 app.run(host='0.0.0.0', port=port, debug=True)
 	#app.run(debug=True)
